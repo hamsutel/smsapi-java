428a5dc711fef32c8b3bef4b5cbd465613be9633
Segbedzi Edem <segbedzi.edem@gmail.com>
2018-10-19 15:44:21 +0000

    Readme added

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -4,14 +4,13 @@ import sun.util.resources.cldr.agq.CurrencyNames_agq;
 
 import java.io.IOException;
 
-public class Main {
+public class Sample {
 
     public static void main(String args[]) throws IOException {
-        System.out.println("Hello World");
-        MessageApi messageApi = new MessageApi("cris@cris.com","cris@cris.com");
+        MessageApi messageApi = new MessageApi("******","******");
         Message message = new Message();
-        message.setSenderId("soso");
-        message.setTo("0542024401");
+        message.setSenderId("Hamsutel");
+        message.setTo("0244111111");
         message.setToken("r6mi9okuqsgg00s80gww9704b54a5f");
         message.setContent("Welcome Home");
         messageApi.send(message);
